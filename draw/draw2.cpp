2b57958d2a3ef085fb7c58295fb15993541af767
діма богинській <dimon070699@gmail.com>
2018-06-14 02:01:39 +0200

    +Auto
    ide spac(((

--- draw/draw2.cpp
+++ draw/draw2.cpp
@@ -5,6 +5,7 @@
 #include "draw2.h"
 #include <vector>
 #include <cstdio>
+#include <cmath>
 
 
 #define MAX_LOADSTRING 100
@@ -19,6 +20,7 @@ INT value;
 
 // buttons
 HWND hwndButton;
+bool Auto = false;
 bool Zlap = false;
 bool Zlapane = false;
 double Pi = 3.14;
@@ -32,17 +34,19 @@ int col = 0;
 int Ox = 250;
 int Oy = 250;
 int Ax; int Ay; int Bx; int By;
+int dima = 0;
 
 int warstwa[250] = { 0 };
 int numer_klockaX = 1;
 int numer_klockaY = 0;
 
 int klocek_1[13] = { 65,220,100,220,135,220,170,220,205,220,240,220 };
-
+int miejsce[2] = { 350,220 };
+double kont_2 = 0;
 
 std::vector<Point> data;
 RECT drawArea1 = { 0, 0, 150, 200 };
-RECT drawArea2 = { 50, 400, 650, 422 };
+RECT drawArea2 = { 0, 0, 500, 500 };
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
@@ -50,16 +54,107 @@ BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	Buttons(HWND, UINT, WPARAM, LPARAM);
+void repaintWindow(HWND hWnd, HDC &hdc, PAINTSTRUCT &ps, RECT *drawArea);
 
+
+void Auto_true(HDC hdc)
+{
+	int polozenie[2];
+	double BC;
+	double kont;
+
+	/*for (int i = 0; i < 6; i++)
+	{*/
+		
+		//kont = sqrt(1 - pow(kont, 2));
+		if (Zlapane == false)
+		{
+			BC = sqrt(pow((klocek_1[(2 * dima)] + 20 - Ox), 2) + (pow((klocek_1[(2 * dima + 1)] +20 - Oy), 2)));
+			BC = ceil(BC);
+			kont = BC / 2 / Ramie;
+			kont = acos(kont) * 180 / Pi ;
+			kont = ceil(kont);
+			if (Alfa <= 175 - kont)
+			{
+				Alfa += 1;
+
+			}
+			else
+			{
+				if (By != klocek_1[(2 * dima + 1)] + 10)
+				{
+					Beta += 1;
+
+				}
+				else
+				{
+					Zlap = true;
+					Zlapane = true;
+				}
+			}
+		}
+		else 
+		{
+			BC = sqrt(pow((miejsce[0] - Ox), 2) + (pow((miejsce[1] - Oy), 2)));
+			BC = ceil(BC);
+			kont = BC / 2 / Ramie;
+			kont = 90- acos(kont) * 180 / Pi/2 ;
+			kont = ceil(kont);
+			if (Beta >= 90)
+			{
+				Beta --;
+
+			}
+			else
+			{
+				if (Alfa >= kont)
+				{
+					Alfa--;
+
+				}
+				else
+				{
+					if (Bx !=  miejsce[0])
+					{
+						Beta--;
+
+					}
+					else
+					{
+
+					 klocek_1[(2 * dima)] = miejsce[0];
+					 klocek_1[(2 * dima+1)] = miejsce[1];
+
+						Zlap = false;
+						Zlapane = false;
+						miejsce[1] = miejsce[1] - 30;
+						numer_klockaY += 2;
+						numer_klockaX += 2;
+						dima++;
+						
+					}
+				}
+
+			}
+			
+
+		}
+		
+
+		
+
+	//}
+}
 
 void MyOnPaint(HDC hdc)
-{
+{ 
+	
 	Graphics graphics(hdc);
 	Pen pen(Color(255, 0, 0, 255));
 	Pen pen2(Color(255, 255, 0, 0));
 
-	graphics.DrawLine(&pen, 0, 250, 500, 250); 
-	graphics.DrawLine(&pen, 250, 0, 250, 500);
+	graphics.DrawLine(&pen, 0, 250, 500, 250);
+	//graphics.DrawLine(&pen, 250, 0, 250, 500);
 
 	Ax = Ramie * cos(Alfa *Pi / 180) + Ox;
 	Ay = -Ramie * sin(Alfa *Pi / 180) + Oy;
@@ -93,11 +188,11 @@ void MyOnPaint(HDC hdc)
 	else
 	{
 		if (numerek == 1 && klocek_1[numer_klockaX] != 220) {
-			
+
 			for (int i = Bx - 250; i < Bx - 220; i++) {
 				if (warstwa[i] > max) {
 					max = warstwa[i];
-				}				
+				}
 			}
 			for (int i = Bx - 250; i < Bx - 220; i++) {
 				warstwa[i] = max + 1;
@@ -115,6 +210,11 @@ void MyOnPaint(HDC hdc)
 		graphics.DrawRectangle(&pen, klocek_1[10], klocek_1[11], 30, 30);
 		graphics.DrawRectangle(&pen2, klocek_1[numer_klockaY], klocek_1[numer_klockaX], 30, 30);
 	}
+	if (Auto == true)
+	{
+		Auto_true(hdc);
+		
+	}
 }
 
 
@@ -127,6 +227,12 @@ void repaintWindow(HWND hWnd, HDC &hdc, PAINTSTRUCT &ps, RECT *drawArea)
 	hdc = BeginPaint(hWnd, &ps);
 	MyOnPaint(hdc);
 	EndPaint(hWnd, &ps);
+	if (dima == 6)
+	{
+		Auto = false;
+		KillTimer(hWnd, TMR_1);
+
+	}
 }
 
 // main function (exe hInstance)
@@ -293,6 +399,24 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 		(HMENU)ID_BUTTON6,                   // the ID of your button
 		hInstance,                            // the instance of your application
 		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("no auto"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		750, 270,                                  // the left and top co-ordinates
+		150, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON7,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("auto"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		590, 270,                                  // the left and top co-ordinates
+		150, 50,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_BUTTON8,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
 
 	hwndButton = CreateWindow(TEXT("button"), TEXT("Zlap klocek "),
 		WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
@@ -385,6 +509,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				numer_klockaX -= 2;
 				numer_klockaY -= 2;
 			}
+			repaintWindow(hWnd, hdc, ps, NULL);
+			break;
+		case ID_BUTTON7:
+			Auto = false;
+			KillTimer(hWnd, TMR_1);
+
+			repaintWindow(hWnd, hdc, ps, NULL);
+			break;
+		case ID_BUTTON8:
+			Auto = true;
+			SetTimer(hWnd, TMR_1, 25, 0);
 			repaintWindow(hWnd, hdc, ps, NULL);
 			break;
 
